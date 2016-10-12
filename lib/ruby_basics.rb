77a48361098577018a51684f3acd63181d6d009e
kevinorta <kevino6532@hstat.org>
2016-10-12 00:19:27 +0000

    fin

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(a,b)
+    return a/b
 end
 
-def argue
-  
+def argue(a)
+  return a
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-    
+    return "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(a="cheese")
+    return a
 end
 
-def whisper
-    
+def whisper(input)
+    return input.downcase
 end
